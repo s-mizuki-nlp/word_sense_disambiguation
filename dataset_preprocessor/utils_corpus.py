375b40c1015c1ca68ae63dcedf438fc23d1a138b
Sakae Mizuki <mizuki.s.aa@m.titech.ac.jp>
2021-05-08 20:23:18 +0900

    feat(implemented Wiki-40b dataset preprocessor.):

--- dataset_preprocessor/utils_corpus.py
+++ dataset_preprocessor/utils_corpus.py
@@ -2,6 +2,7 @@
 # -*- coding:utf-8 -*-
 
 from torchtext.datasets.wikitext103 import _RawTextIterableDataset
+from datasets.arrow_dataset import Dataset
 
 def batch_document_generator_for_wikitext_dataset(wikitext_dataset: _RawTextIterableDataset, batchsize: int):
     txt = ""; idx = 0
@@ -19,3 +20,59 @@ def batch_document_generator_for_wikitext_dataset(wikitext_dataset: _RawTextIter
             yield txt
             txt = ""
             idx = 0
+
+
+def batch_document_generator_for_wiki40b_dataset(wiki40b_dataset: Dataset, batchsize: int, first_paragraph_only: bool):
+    txt = ""; idx = 0
+    for article in wiki40b_dataset:
+        if first_paragraph_only:
+            doc = _extract_first_paragraph_from_wiki40b_text(article["text"])
+        else:
+            doc = _extract_paragraph_from_wiki40b_text(article["text"])
+        if len(doc) <= 10:
+            continue
+
+        txt += doc; txt += "\n"
+        idx += 1
+
+        if idx >= batchsize:
+            yield txt.strip("\n")
+            txt = ""
+            idx = 0
+
+
+def _extract_paragraph_from_wiki40b_text(str_wiki40b_text: str, newline: str = "\n", delimiter: str = "\n"):
+    # strip first/last newlines
+    txt = str_wiki40b_text.strip()
+
+    # split text into sections
+    lst_texts = txt.split("\n")
+
+    # extract paragraphs
+    it_marker_text_tuple = zip(lst_texts[0::2], lst_texts[1::2])
+    it_ret = (text for marker, text in it_marker_text_tuple if marker == "_START_PARAGRAPH_")
+
+    # concat and replace newline symbol
+    return delimiter.join(it_ret).replace("_NEWLINE_", newline)
+
+def _extract_first_paragraph_from_wiki40b_text(str_wiki40b_text: str, newline: str = "\n"):
+    # strip first/last newlines
+    txt = str_wiki40b_text.strip()
+
+    # split text into sections
+    lst_texts = txt.split("\n")
+
+    # extract first paragraph
+    for idx, text in enumerate(lst_texts):
+        # if article begins with section, it indicates there is not first paragraph. thus returns empty string.
+        if text == "_START_SECTION_":
+            return ""
+        if text == "_START_PARAGRAPH_":
+            break
+
+    if idx == len(lst_texts) - 1:
+        return ""
+
+    first_paragraph = lst_texts[idx+1]
+    # replace newline symbol
+    return first_paragraph.replace("_NEWLINE_", newline)
