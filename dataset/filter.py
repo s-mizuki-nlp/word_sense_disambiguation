d6b2d76664ec24e6f97a0c31851643702794f3ae
Sakae Mizuki <mizuki.s.aa@m.titech.ac.jp>
2021-07-03 22:26:02 +0900

    feat: implemented token attribute based record filter.

--- dataset/filter.py
+++ dataset/filter.py
@@ -6,7 +6,7 @@ from __future__ import division
 from __future__ import print_function
 
 
-from typing import Optional, Dict, Set
+from typing import Optional, Dict, Set, List
 
 
 class DictionaryFilter(object):
@@ -31,3 +31,41 @@ class DictionaryFilter(object):
                 if sample[field_name] in values:
                     return False
             return True
+
+class TokenAttributeFilter(object):
+
+    def __init__(self,
+                 includes: Optional[Dict[str, Set[str]]] = None,
+                 excludes: Optional[Dict[str, Set[str]]] = None,
+                 token_field_name: str = "token"):
+        self._token_field_name = token_field_name
+        self._includes = includes
+        self._excludes = excludes
+        assert (includes is not None) or (excludes is not None), f"you must specify either `includes` or `excludes` argument."
+        assert (includes is None) or (excludes is None), f"you can't specify both `includes` and `excludes` at the same time."
+
+    def __call__(self, sample: Dict[str, List[Dict[str,str]]]):
+
+        lst_tokens = sample[self._token_field_name]
+
+        if self._includes is not None:
+            for attribute_name, includes in self._includes.items():
+                it_token_attrs = (token.get(attribute_name, None) for token in lst_tokens)
+                set_attrs = set(filter(bool, it_token_attrs))
+
+                if len(set_attrs.intersection(includes)) > 0:
+                    # 条件に合致すればフィルタリングしない
+                    return False
+
+            return True
+
+        if self._excludes is not None:
+            for attribute_name, excludes in self._excludes.items():
+                it_token_attrs = (token.get(attribute_name, None) for token in lst_tokens)
+                set_attrs = set(filter(bool, it_token_attrs))
+
+                if len(set_attrs.intersection(excludes)) > 0:
+                    # 条件に合致すればフィルタリングする
+                    return True
+
+            return False
