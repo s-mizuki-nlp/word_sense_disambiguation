c81a0893d428ca86a67e2aab52b55af8e8c0303c
Sakae Mizuki <mizuki.s.aa@m.titech.ac.jp>
2021-08-21 22:37:46 +0900

    ci: changed config object name.

--- config_files/evaluation_datasets.py
+++ config_files/evaluation_datasets.py
@@ -11,10 +11,11 @@ from dataset.transform import FieldTypeConverter
 DIR_EVALSET = "/home/sakae/Windows/dataset/word_sense_disambiguation/WSD_Evaluation_Framework/Evaluation_Datasets/"
 
 # evaluation dataset for all-words WSD task
-cfg_evaluation_datasets_word_sense_disambiguation = {
+word_sense_disambiguation = {
     "ALL": {
-        "path_data": os.path.join(DIR_EVALSET, "ALL/ALL.data.xml"),
-        "path_gold": os.path.join(DIR_EVALSET, "ALL/ALL.gold.key.txt"),
+        "path_corpus": os.path.join(DIR_EVALSET, "ALL/ALL.data.xml"),
+        "path_ground_truth_labels": os.path.join(DIR_EVALSET, "ALL/ALL.gold.key.txt"),
+        "lookup_candidate_senses": True,
         "description": "WSD dataset: ALL",
     }
 }
