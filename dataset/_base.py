06eb64d499036f9dc8dc8811109faa93ae12f6fa
Sakae Mizuki <mizuki.s.aa@m.titech.ac.jp>
2021-08-26 18:52:34 +0900

    refactor: pulled some members up to super class.

--- dataset/_base.py
+++ dataset/_base.py
@@ -11,7 +11,7 @@ from torch.utils.data import IterableDataset
 from dataset_preprocessor import utils
 
 
-class AbstractFormatDataset(IterableDataset, metaclass=ABCMeta):
+class AbstractFormatDataset(metaclass=ABCMeta):
 
     def __init__(self, path: str,
                  transform_functions=None,
--- dataset/corpora.py
+++ dataset/corpora.py
@@ -6,8 +6,9 @@ from typing import Union, Collection, Optional, Dict, Any, Iterable, Callable, L
 from torch.utils.data import IterableDataset
 
 from dataset_preprocessor import utils
+from ._base import AbstractFormatDataset
 
-class NDJSONDataset(IterableDataset):
+class NDJSONDataset(AbstractFormatDataset, IterableDataset):
 
     def __init__(self, path: str,
                  binary: bool,
@@ -28,25 +29,9 @@ class NDJSONDataset(IterableDataset):
         :param kwargs_for_json_loads: `json.loads()` methodに渡すキーワード引数．binary=Trueの場合は無視．
         """
 
-        super().__init__()
-        self._path = path
+        super().__init__(path, transform_functions, filter_function, n_rows, description)
         self._binary = binary
         self._kwargs_for_json_loads = kwargs_for_json_loads
-
-        assert os.path.exists(path), f"invalid path specified: {path}"
-
-        self._description = description
-        self._transform_functions = transform_functions
-
-        if filter_function is None:
-            self._filter_function = []
-        elif isinstance(filter_function, list):
-            self._filter_function = filter_function
-        elif not isinstance(filter_function, list):
-            self._filter_function = [filter_function]
-
-        self._n_rows = n_rows
-        self._n_sample = None
 
     def _record_loader_text(self):
         iter_records = io.open(self._path, mode="r")
@@ -64,74 +49,6 @@ class NDJSONDataset(IterableDataset):
             return self._record_loader_binary()
         else:
             return self._record_loader_text()
-
-    def _apply(self, apply_field_name: str, apply_function: Callable, na_value: Optional[Any] = None):
-
-        _transform_cache = self._transform_functions
-        self._transform_functions = None
-
-        it = (entry.get(apply_field_name, na_value) for entry in self)
-        ret =  apply_function(filter(bool, it))
-
-        self._transform_functions = _transform_cache
-        return ret
-
-    def distinct_values(self, column: str) -> List[str]:
-        return self._apply(apply_field_name=column, apply_function=lambda it: list(set(it)))
-
-    def iter_specific_field(self, field_name: str, na_value: Optional[Any] = None):
-        for entry in self:
-            yield entry.get(field_name, na_value)
-
-    def __len__(self):
-        if self._n_sample is not None:
-            return self._n_sample
-
-        n_sample = 0
-        for _ in self:
-            n_sample += 1
-
-        self._n_sample = n_sample
-
-        return n_sample
-
-    def _transform(self, entry: Dict[str, Any]):
-        if self._transform_functions is None:
-            return entry
-
-        for field_name, transform_function in self._transform_functions.items():
-            entry[field_name] = transform_function(entry[field_name])
-
-        return entry
-
-    def _filter(self, entry: Dict[str, Any]):
-        for filter_function in self._filter_function:
-            if filter_function(entry) == True:
-                return True
-        return False
-
-    def __iter__(self):
-        if isinstance(self._transform_functions, dict):
-            for field_name, function in self._transform_functions.items():
-                if hasattr(function, "reset"):
-                    function.reset()
-
-        iter_records = self._record_loader()
-        n_read = 0
-        for record in iter_records:
-            # transform each field of the entry
-            entry = self._transform(record)
-
-            # verify the entry is valid or not
-            if self._filter(entry) == True:
-                continue
-
-            yield entry
-
-            n_read += 1
-            if self._n_rows is not None:
-                if n_read >= self._n_rows:
-                    break
 
     @property
     def verbose(self):
