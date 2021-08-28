59ef45b9500a2b5be5f3c781a35083fd3ddf73a0
Sakae Mizuki <mizuki.s.aa@m.titech.ac.jp>
2021-08-28 22:33:52 +0900

    refactor: updated internal method usage.

--- dataset/contextualized_embeddings.py
+++ dataset/contextualized_embeddings.py
@@ -7,7 +7,7 @@ import h5py
 
 from torch.utils.data import IterableDataset
 from ._base import AbstractFormatDataset
-from .encoder import convert_compressed_format_to_batch_format
+from .encoder import convert_compressed_format_to_list_of_tensors
 
 
 class BERTEmbeddingsBatchDataset(AbstractFormatDataset, IterableDataset):
@@ -39,6 +39,7 @@ class BERTEmbeddingsDataset(BERTEmbeddingsBatchDataset):
 
     def __init__(self,
                  path: str,
+                 padding: bool = False,
                  max_sequence_length: Optional[int] = None,
                  transform_functions=None,
                  filter_function: Optional[Union[Callable, List[Callable]]] = None,
@@ -46,6 +47,7 @@ class BERTEmbeddingsDataset(BERTEmbeddingsBatchDataset):
                  description: str = ""):
 
         super().__init__(path, transform_functions, filter_function, n_rows, description)
+        self._padding = padding
         self._max_sequence_length = max_sequence_length
 
     def _record_loader(self):
@@ -53,21 +55,17 @@ class BERTEmbeddingsDataset(BERTEmbeddingsBatchDataset):
         iter_batch = super()._record_loader()
 
         for batch in iter_batch:
-            # embeddings: (n_batch, max_seq_len, n_dim)
-            dict_ = convert_compressed_format_to_batch_format(embeddings=batch["embeddings"],
-                                                                   lst_sequence_lengths=batch["sequence_lengths"],
-                                                                   max_sequence_length=self._max_sequence_length,
-                                                                   return_tensor=False)
-            embeddings = dict_["embeddings"]
-            attention_masks = dict_["attention_mask"]
+            lst_embeddings = convert_compressed_format_to_list_of_tensors(embeddings=batch["embeddings"],
+                                                                 lst_sequence_lengths=batch["sequence_lengths"],
+                                                                 padding=self._padding,
+                                                                 max_sequence_length=self._max_sequence_length)
             lst_records = batch["records"]
 
-            iter_records = zip(embeddings, batch["sequence_lengths"], attention_masks, lst_records)
-            for embedding, seq_len, attention_mask, record in iter_records:
+            iter_records = zip(lst_embeddings, batch["sequence_lengths"], lst_records)
+            for embedding, seq_len, record in iter_records:
                 dict_record = {
                     "embedding": embedding,
                     "sequence_length": seq_len,
-                    "attention_mask": attention_mask,
                     "record": record
                 }
                 yield dict_record
