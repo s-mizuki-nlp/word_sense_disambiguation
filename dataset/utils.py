f7da5671cb38d2885a35c300fb421083a2358626
Sakae Mizuki <mizuki.s.aa@m.titech.ac.jp>
2021-09-01 21:33:02 +0900

    feat: implemented mask generation functions

--- dataset/utils.py
+++ dataset/utils.py
@@ -57,3 +57,79 @@ def pad_trailing_tensors(embeddings: torch.Tensor, n_length_after_padding: int):
     embeddings_padded = padding_function(embeddings)
 
     return embeddings_padded
+
+def pad_and_stack_list_of_tensors(lst_embeddings: List[torch.Tensor], max_sequence_length: Optional[int] = None,
+                                  return_sequence_length: bool = False):
+    """
+    it takes the list of embeddings as the input, then applies zero-padding and stacking to transform it as
+
+    @param lst_embeddings:
+    @param max_sequence_length:
+    """
+    dim = -2 # second last axis. it must be the sequence dimension.
+
+    lst_seq_len = [embeddings.shape[dim] for embeddings in lst_embeddings]
+    if max_sequence_length is None:
+        max_sequence_length = max(lst_seq_len)
+    else:
+        n_max = max(lst_seq_len)
+        assert max_sequence_length >= n_max, \
+            f"`max_sequence_length` must be greater or equal to max. embeddings size: {n_max} > {max_sequence_length}"
+
+    lst_padded_embeddings = [pad_trailing_tensors(e_t, max_sequence_length) for e_t in lst_embeddings]
+    stacked_embeddings = torch.stack(lst_padded_embeddings)
+
+    if return_sequence_length:
+        return stacked_embeddings, lst_seq_len
+    else:
+        return stacked_embeddings
+
+def create_multiheadattention_attn_mask(
+        query_sequence_length: int, key_value_sequence_length: int,
+        target_sequence_length: int, source_sequence_length: int,
+        num_heads: int, device: Optional[torch.device] = None):
+    params = {
+        "size": (num_heads, target_sequence_length, source_sequence_length),
+        "fill_value": True,
+        "dtype": torch.bool
+    }
+    if device is not None:
+        params["device"] = device
+
+    attn_mask = torch.full(**params)
+    attn_mask[:, :query_sequence_length, :key_value_sequence_length] = False
+    return attn_mask
+
+def create_multiheadattention_attn_mask_batch(
+        lst_query_sequence_lengths: List[int], lst_key_value_sequence_lengths: List[int],
+        target_sequence_length: int, source_sequence_length: int,
+        num_heads: int, device: Optional[torch.device] = None):
+
+    def _apply_attn_mask(n_query, n_key_value):
+        return create_multiheadattention_attn_mask(query_sequence_length=n_query, key_value_sequence_length=n_key_value,
+                                                   target_sequence_length=target_sequence_length, source_sequence_length=source_sequence_length,
+                                                   num_heads=num_heads, device=device)
+
+    lst_attn_masks = [_apply_attn_mask(n_q, n_kv) for n_q, n_kv in zip(lst_query_sequence_lengths, lst_key_value_sequence_lengths)]
+    attn_masks = torch.cat(lst_attn_masks, dim=0)
+
+    return attn_masks
+
+def create_sequence_mask(lst_sequence_lengths: List[int], max_sequence_length: Optional[int] = None,
+                         device: Optional[torch.device] = None):
+    max_sequence_length = max(lst_sequence_lengths) if max_sequence_length is None else max_sequence_length
+    n_seq = len(lst_sequence_lengths)
+
+    params = {
+        "size": (n_seq, max_sequence_length),
+        "fill_value": True,
+        "dtype": torch.bool
+    }
+    if device is not None:
+        params["device"] = device
+
+    seq_mask = torch.full(**params)
+    for seq_idx, seq_len in enumerate(lst_sequence_lengths):
+        seq_mask[seq_idx, :seq_len] = False
+
+    return seq_mask
