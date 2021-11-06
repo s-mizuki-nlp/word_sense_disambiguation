b7c2c637e5f6215585443c46c89dad23868e1fe9
Sakae Mizuki <mizuki.s.aa@m.titech.ac.jp>
2021-11-06 21:51:06 +0900

    feat: [WIP] implementing transformer encoder. todo: inference loop

--- model/encoder.py
+++ model/encoder.py
@@ -1,51 +1,25 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 import warnings
-from typing import Optional, Dict, Any, Union, Tuple
+from typing import Optional, Dict, Any, Union, Tuple, List, Iterable
+
+import numpy as np
 import torch
 from torch import nn
 from torch.nn import functional as F
 
 from .loss_unsupervised import CodeValueMutualInformationLoss
 from .onmt.global_attention import GlobalAttention
+from .encoder_internal import PositionalEncoding
 
-class SimpleEncoder(nn.Module):
+class BaseEncoder(nn.Module):
 
-    def __init__(self, n_dim_emb: int, n_digits: int, n_ary: int,
-                 n_dim_hidden: Optional[int] = None,
-                 dtype=torch.float32, **kwargs):
-
-        super(SimpleEncoder, self).__init__()
-
-        self._n_dim_emb = n_dim_emb
-        self._n_dim_hidden = int(n_digits*n_ary //2) if n_dim_hidden is None else n_dim_hidden
-        self._n_digits = n_digits
+    def __init__(self, n_ary: int):
+        super().__init__()
         self._n_ary = n_ary
-        self._dtype = dtype
 
-        self._build()
-
-    def _build(self):
-
-        self.x_to_h = nn.Linear(in_features=self._n_dim_emb, out_features=self._n_dim_hidden)
-        self.lst_h_to_z = nn.ModuleList([nn.Linear(in_features=self._n_dim_hidden, out_features=self._n_ary) for n in range(self._n_digits)])
-
-    def forward(self, input_x: torch.Tensor):
-
-        t_h = torch.tanh(self.x_to_h(input_x))
-        lst_z = [torch.log(F.softplus(h_to_z(t_h))) for h_to_z in self.lst_h_to_z]
-        lst_prob_c = [F.softmax(t_z, dim=1) for t_z in lst_z]
-
-        t_prob_c = torch.stack(lst_prob_c, dim=1)
-
-        return t_prob_c
-
-    def calc_code_probability(self, input_x: torch.Tensor, **kwargs):
-        return self.forward(input_x)
-
-    @property
-    def has_discretizer(self):
-        return False
+    def _dtype_and_device(self, t: torch.Tensor):
+        return t.dtype, t.device
 
     def _adjust_code_probability_to_monotone_increasing(self, probs: torch.Tensor, probs_prev: Union[None, torch.Tensor],
                                                         eps: float = 1E-6):
@@ -74,7 +48,7 @@ class SimpleEncoder(nn.Module):
         return probs_adj
 
 
-class LSTMEncoder(SimpleEncoder):
+class LSTMEncoder(BaseEncoder):
 
     def __init__(self, n_dim_emb: int, n_digits: int, n_ary: int,
                  n_dim_hidden: Optional[int] = None,
@@ -86,10 +60,9 @@ class LSTMEncoder(SimpleEncoder):
                  code_embeddings_type: str = "time_distributed",
                  trainable_beginning_of_code: bool = True,
                  prob_zero_monotone_increasing: bool = False,
-                 dtype=torch.float32,
                  **kwargs):
 
-        super(SimpleEncoder, self).__init__()
+        super(BaseEncoder, self).__init__()
 
         if teacher_forcing:
             if discretizer is not None:
@@ -105,7 +78,6 @@ class LSTMEncoder(SimpleEncoder):
         self._n_dim_hidden = n_dim_emb if n_dim_hidden is None else n_dim_hidden
         self._n_digits = n_digits
         self._n_ary = n_ary
-        self._dtype = dtype
         self._n_ary_internal = None
         self._global_attention_type = global_attention_type
         self._teacher_forcing = teacher_forcing
@@ -160,9 +132,6 @@ class LSTMEncoder(SimpleEncoder):
             self._h_to_z = nn.Linear(in_features=self._n_dim_hidden*2, out_features=self._n_ary, bias=True)
         else: # z_t = FF(h_t;\theta_t)
             self._h_to_z = nn.Linear(in_features=self._n_dim_hidden, out_features=self._n_ary, bias=True)
-
-    def _dtype_and_device(self, t: torch.Tensor):
-        return t.dtype, t.device
 
     def _init_states(self, n_batch: int, dtype, device):
         h_0 = torch.zeros((n_batch, self._n_dim_hidden), dtype=dtype, device=device)
@@ -331,125 +300,176 @@ class LSTMEncoder(SimpleEncoder):
         return ret
 
 
-class TransformerEncoder(SimpleEncoder):
+class TransformerEncoder(BaseEncoder):
 
     def __init__(self, n_dim_emb: int, n_digits: int, n_ary: int,
-                 normalize_digit_embeddings: bool,
-                 n_layer: int = 4,
-                 n_head: int = 4,
+                 n_layer: int, n_head: int,
+                 pos_tagset: Iterable[str],
+                 discretizer: Optional[nn.Module] = None,
+                 norm_digit_embeddings: bool = False,
+                 ignore_trailing_zero_digits: bool = False,
+                 trainable_positional_encoding: bool = False,
                  dropout: float = 0.1,
-                 n_dim_emb_digits: Optional[int] = None,
-                 time_distributed: bool = True,
-                 how_digit_embeddings: str = "add",
+                 batch_first: bool = True,
                  prob_zero_monotone_increasing: bool = False,
-                 dtype=torch.float32,
                  **kwargs):
 
-        raise NotImplementedError(f"not implemented yet.")
-
-        super(SimpleEncoder, self).__init__()
+        super().__init__()
 
         self._n_dim_emb = n_dim_emb
-        self._n_dim_emb_digits = n_dim_emb if n_dim_emb_digits is None else n_dim_emb_digits
         self._n_digits = n_digits
         self._n_ary = n_ary
+        self._pos_tagset = set(pos_tagset)
+        self._discretizer = discretizer
         self._n_layer = n_layer
         self._n_head = n_head
         self._dropout = dropout
-        self._dtype = dtype
-        self._time_distributed = time_distributed
-        self._how_digit_embeddings = how_digit_embeddings
-        self._normalize_digit_embeddings = normalize_digit_embeddings
+        self._norm_digit_embeddings = norm_digit_embeddings
+        self._ignore_trailing_zero_digits = ignore_trailing_zero_digits
+        self._trainable_positional_encoding = trainable_positional_encoding
+        self._batch_first = batch_first
         self._prob_zero_monotone_increasing = prob_zero_monotone_increasing
 
         self._build()
 
     def _build(self):
 
+        # assign part-of-speech index
+        self._pos_index = {}
+        for idx, pos in enumerate(self._pos_tagset):
+            self._pos_index[pos] = self._n_ary + idx
+
         # digit embeddings
-        cfg_embedding_layer = {
-            "num_embeddings":self._n_digits,
-            "embedding_dim":self._n_dim_emb_digits,
-            "max_norm":1.0 if self._normalize_digit_embeddings else None
+        cfg_emb_layer = {
+            "num_embeddings":self._n_ary + len(self._pos_tagset), # n_pos will be used for beginning-of-sense-code symbol.
+            "embedding_dim":self._n_dim_emb,
+            "max_norm":1.0 if self._norm_digit_embeddings else None,
+            "padding_idx":0 if self._ignore_trailing_zero_digits else None
         }
-        self._embedding_digit = nn.Embedding(**cfg_embedding_layer)
+        self._emb_layer = nn.Embedding(**cfg_emb_layer)
 
-        if self._how_digit_embeddings == "add":
-            n_dim_transformer = self._n_dim_emb
-        elif self._how_digit_embeddings == "concat":
-            n_dim_transformer = self._n_dim_emb + self._n_dim_emb_digits
-        else:
-            raise NotImplementedError(f"unknown `how_digit_embeddings` value: {self._how_digit_embeddings}")
+        # positional encodings
+        cfg_pe_layer = {
+            "d_model":self._n_dim_emb,
+            "dropout":self._dropout,
+            "max_len":self._n_digits,
+            "trainable":self._trainable_positional_encoding
+        }
+        self._pe_layer = PositionalEncoding(**cfg_pe_layer)
 
-        # transformer layers
-        cfg_transformer_layer = {
-            "d_model":n_dim_transformer,
+        # transformer decoder
+        cfg_transformer_decoder_layer = {
+            "d_model":self._n_dim_emb,
             "nhead":self._n_head,
-            "dim_feedforward":n_dim_transformer*4,
+            "dim_feedforward":self._n_dim_emb*4,
             "dropout":self._dropout
         }
-        layer = nn.TransformerEncoderLayer(**cfg_transformer_layer)
-        self._transformer = nn.TransformerEncoder(layer, num_layers=self._n_layer)
+        layer = nn.TransformerDecoderLayer(**cfg_transformer_decoder_layer)
+        self._decoder = nn.TransformerDecoder(layer, num_layers=self._n_layer)
 
-        # prediction layer
-        if self._time_distributed:
-            self._prediction_layer = nn.Linear(in_features=n_dim_transformer, out_features=self._n_ary, bias=True)
-        else:
-            lst_layers = [nn.Linear(in_features=n_dim_transformer, out_features=self._n_ary, bias=True) for _ in range(self._n_digits)]
-            self._prediction_layer = nn.ModuleList(lst_layers)
+        # logits layer
+        self._softmax_logit_layer = nn.Linear(in_features=self._n_dim_emb, out_features=self._n_ary, bias=True)
 
-    def _dtype_and_device(self, t: torch.Tensor):
-        return t.dtype, t.device
+    def create_teacher_forcing_inputs(self, ground_truth_synset_codes: torch.Tensor, lst_pos: List[str]):
+        n_batch, n_digits = ground_truth_synset_codes.shape
+        dtype, device = self._dtype_and_device(ground_truth_synset_codes)
 
-    def forward(self, input_x: torch.Tensor, on_inference: bool = False):
-        dtype, device = self._dtype_and_device(input_x)
+        # prepend PoS index, trim tail digit.
+        lst_pos_idx = [self._pos_index[pos] for pos in lst_pos]
+        # t_boc: (n_batch, 1)
+        t_boc = torch.LongTensor(lst_pos_idx, device=device).unsqueeze(dim=-1)
+        # t_inputs: (n_batch, n_digits)
+        t_inputs = torch.cat((t_boc, ground_truth_synset_codes[:,:n_digits-1]))
+        return t_inputs
 
-        # input_x: (N_batch, N_dim_emb)
-        n_batch = input_x.shape[0]
+    def subsequent_mask(self, n_seq_length: int):
+        "Mask out subsequent positions of target embeddings."
+        attn_shape = (n_seq_length, n_seq_length)
+        subsequent_mask = np.triu(np.ones(attn_shape), k=1).astype('uint8')
+        return torch.from_numpy(subsequent_mask) == 1
 
-        # transformer layer inputs
-        # x_in: (N_batch, N_digits, N_dim_transformer)
+    def forward_training(self, pos: List[str], entity_embeddings: torch.Tensor,
+                entity_sequence_mask: torch.Tensor,
+                ground_truth_synset_codes: Optional[torch.Tensor] = None,
+                **kwargs):
+        # crate inputs
+        t_inputs = self.create_teacher_forcing_inputs(ground_truth_synset_codes, pos)
 
-        ## entity embeddings
-        x_in_entity = input_x.unsqueeze(1).repeat(1, self._n_digits, 1)
-        ## digit embeddings
-        dummy_digits = torch.arange(self._n_digits, dtype=torch.long, device=device)
-        x_in_digits = self._embedding_digit.forward(dummy_digits).unsqueeze(0).repeat(n_batch,1,1)
-        ## merge two embeddings
-        if self._how_digit_embeddings == "add":
-            x_in = x_in_entity + x_in_digits
-        elif self._how_digit_embeddings == "concat":
-            x_in = torch.cat((x_in_entity, x_in_digits), dim=-1)
-        else:
-            raise NotImplementedError(f"unknown `how_digit_embeddings` value: {self._how_digit_embeddings}")
+        # compute target embeddings: (n_batch, n_digits, n_dim_emb)
+        tgt = self._pe_layer.forward( self._emb_layer.forward(t_inputs) )
+        # prepare subsequent masks: (n_digits, n_digits)
+        tgt_mask = self.subsequent_mask(n_seq_length=self._n_digits)
 
-        # transformer
-        # transformer's input shape must be (N_digits, N_batch, N_dim_transformer)
-        # h_out: (N_batch, N_digits, N_dim_transformer)
-        h_out = self._transformer.forward(x_in.transpose(1,0)).transpose(0,1)
+        # prepare memory embeddings and masks
+        memory = entity_embeddings
+        memory_key_padding_mask = entity_sequence_mask
 
-        # compute Pr{c_d}
-        # t_prob_c: (n_batch, n_digits, n_ary)
-        if self._time_distributed:
-            lst_prob_c = [F.softmax(self._prediction_layer(h_out[:,idx_d,:]), dim=-1) for idx_d in range(self._n_digits)]
-        else:
-            lst_prob_c = [F.softmax(layer(h_out[:,idx_d,:]), dim=-1) for idx_d, layer in enumerate(self._prediction_layer)]
+        if self._batch_first:
+            tgt = tgt.swapaxes(0, 1)
+            memory = memory.swapaxes(0, 1)
+
+        # compute transformer decoder
+        h_out = self._decoder.forward(tgt=tgt, tgt_mask=tgt_mask, memory=memory, memory_key_padding_mask=memory_key_padding_mask)
+        if self._batch_first:
+            h_out = h_out.swapaxes(0, 1)
+
+        # compute Pr{Y_d|y_{<d}}
+        # lst_code_probs: List[tensor(n_batch, n_ary)]
+        lst_code_probs = [F.softmax(self._softmax_logit_layer(h_out[:, idx_d, :]), dim=-1) for idx_d in range(self._n_digits)]
 
         # adjust Pr{c_d=d|c_{<d}} so that Pr{c_d=0|c_{<d+1}} satisfies monotone increasing condition.
         if self._prob_zero_monotone_increasing:
             for d in range(self._n_digits):
-                prob_c_prev = lst_prob_c[d-1] if d > 0 else None
-                t_prob_c_d = lst_prob_c[d]
-                lst_prob_c[d] = self._adjust_code_probability_to_monotone_increasing(probs=t_prob_c_d, probs_prev=prob_c_prev)
+                prob_c_prev = lst_code_probs[d-1] if d > 0 else None
+                t_prob_c_d = lst_code_probs[d]
+                lst_code_probs[d] = self._adjust_code_probability_to_monotone_increasing(probs=t_prob_c_d, probs_prev=prob_c_prev)
 
-        # stack each digits
-        t_prob_c = torch.stack(lst_prob_c, dim=1)
+        # stack all digits
+        # t_code_prob: (n_batch, n_digits, n_ary)
+        t_code_prob = torch.stack(lst_code_probs, dim=1)
 
-        return t_prob_c
+        # sample continuous relaxed codes
+        if self.has_discretizer:
+            t_latent_code = self._discretizer.forward(t_code_prob)
+        else:
+            t_latent_code = t_code_prob
 
-    def calc_code_probability(self, input_x: torch.Tensor, adjust_code_probability: bool = False, **kwargs):
-        t_prob_c = self.forward(input_x, on_inference=True)
-        if adjust_code_probability:
-            t_prob_c = CodeValueMutualInformationLoss.calc_adjusted_code_probability(t_prob_c)
+        return t_latent_code, t_code_prob
 
-        return t_prob_c
+    def forward_inference_greedy(self, pos: List[str], entity_embeddings: torch.Tensor,
+                                 entity_sequence_mask: torch.Tensor,
+                                 apply_argmax_on_inference: bool,
+                                 **kwargs):
+        return None, None
+
+    def forward(self, pos: List[str], entity_embeddings: torch.Tensor,
+                entity_sequence_mask: torch.Tensor,
+                ground_truth_synset_codes: Optional[torch.Tensor] = None,
+                on_inference: bool = False,
+                apply_argmax_on_inference: bool = False,
+                **kwargs):
+
+        if not on_inference:
+            return self.forward_training(pos, entity_embeddings, entity_sequence_mask, ground_truth_synset_codes, **kwargs)
+        else:
+            return self.forward_inference_greedy(pos, entity_embeddings, entity_sequence_mask, apply_argmax_on_inference, **kwargs)
+
+    @property
+    def has_discretizer(self):
+        return self._discretizer is not None
+
+    @property
+    def discretizer(self):
+        return self._discretizer
+
+    @property
+    def n_digits(self):
+        return self._n_digits
+
+    @property
+    def n_ary(self):
+        return self._n_ary
+
+    @property
+    def teacher_forcing(self):
+        return True
