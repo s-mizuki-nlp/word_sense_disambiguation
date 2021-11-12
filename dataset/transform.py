4352d3b5e64ba741a88e12a9b2cc278d5c9bdd1b
Sakae Mizuki <mizuki.s.aa@m.titech.ac.jp>
2021-11-12 18:45:03 +0900

    refactor: removed unused transform functions.

--- dataset/transform.py
+++ dataset/transform.py
@@ -21,39 +21,6 @@ def top_digit_remover(synset_code: List[int]):
 def top_digits_remover(synset_codes: List[List[int]]):
     return list(map(top_digit_remover, synset_codes))
 
-class EmbeddingNormalizer(object):
-
-    def __init__(self, field_name_embedding="embedding"):
-        self._field_name = field_name_embedding
-
-    def __call__(self, sample):
-
-        x = sample[self._field_name]
-        sample[self._field_name] = x / np.linalg.norm(x)
-
-        return sample
-
-
-class HyponymyEntryToListOfHyponymyPair(object):
-
-    def __init__(self, field_name_hyponym: str = "hyponym", field_name_hypernym: str = "hypernym", field_name_hypernyms: str = "hypernyms"):
-        self._field_name_hyponym = field_name_hyponym
-        self._field_name_hypernym = field_name_hypernym
-        self._field_name_hypernyms = field_name_hypernyms
-
-    def __call__(self, sample):
-        hyponym = sample.get(self._field_name_hyponym, None)
-        hypernym = sample.get(self._field_name_hypernym, None)
-        hypernyms = sample.get(self._field_name_hypernyms, None)
-
-        assert hyponym is not None, "could not get hyponym."
-
-        if hypernym is not None:
-            return [(hyponym, hypernym)]
-        elif isinstance(hypernyms, list):
-            return [(hyponym, hyper) for hyper in hypernyms]
-        else:
-            raise AssertionError("could not get hypernym.")
 
 class FieldTypeConverter(object):
 
