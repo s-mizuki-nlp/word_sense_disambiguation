0e53dea8b4cf4e5245ac61e1c65c8cb7ab414949
Sakae Mizuki <mizuki.s.aa@m.titech.ac.jp>
2021-12-03 22:34:13 +0900

    feat: [WIP] implementing sense code statistics

--- model/encoder_internal.py
+++ model/encoder_internal.py
@@ -10,7 +10,7 @@ from torch import nn
 import math
 
 from .hashembed.embedding import HashFamily
-
+from collections import Counter, defaultdict
 
 class BaseHashCode(torch.nn.Module):
 
@@ -20,6 +20,7 @@ class BaseHashCode(torch.nn.Module):
         self._sequence_hash_function = HashFamily(bins=n_prefix_hash_bins, mask_zero=mask_zero, is_sequence_input=True,
                                                   max_seq_len=max_seq_len, random_seed=random_seed).draw_hash()
         self._pad_trailing_zeroes = pad_trailing_zeroes
+        self._prefix_stats = defaultdict(Counter)
 
     def transform_sequence_to_prefix_hashes(self, sequences: torch.Tensor):
         """
@@ -47,6 +48,26 @@ class BaseHashCode(torch.nn.Module):
                 prefix_ids[idx, code_length:] = prefix_ids[idx, code_length-1]
 
         return prefix_ids
+
+    def collect_prefix_stats(self, ground_truth_synset_codes: torch.Tensor, input_sequences: torch.Tensor):
+        sequence_prefix_hashes = self.transform_sequence_to_prefix_hashes(input_sequences)
+        it_seq = zip(ground_truth_synset_codes.tolist(), sequence_prefix_hashes.tolist())
+        for ground_truth_synset_code, sequence_prefix_hash in it_seq:
+            for next_value, prefix_hash in zip(sequence_prefix_hash):
+                self._prefix_stats[prefix_hash][next_value] += 1
+
+    def lookup_prior_probability(self, input_sequences: torch.Tensor, num_classes: int, smoothing_alpha: int = 10) -> torch.Tensor:
+        """
+        returns class-prior probability of each position in the sequence based on prefix statistics.
+
+        @param input_sequences: (n_batch, n_digits)
+        @param num_classes: number of classes. i.e., n_ary
+        @param smoothing_alpha: smoothing parameter for zero-freq entry.
+        """
+        sequence_prefix_hashes = self.transform_sequence_to_prefix_hashes(input_sequences)
+        for sequence_prefix_hash in sequence_prefix_hashes.tolist():
+            pass
+
 
 
 class PositionalEncoding(torch.nn.Module):
