d4876f76774a734bffe2867f0d1fd6ebb569edc4
Sakae Mizuki <mizuki.s.aa@m.titech.ac.jp>
2021-12-24 21:45:22 +0900

    ci: added missing argument.

--- config_files/wsd_task.py
+++ config_files/wsd_task.py
@@ -85,6 +85,7 @@ cfg_task_dataset = {
         "return_level":"entity",
         "record_entity_field_name":"entities",
         "record_entity_span_field_name":"subword_spans",
+        "ground_truth_lemma_keys_field_name":"ground_truth_lemma_keys",
         "copy_field_names_from_record_to_entity":None,
         "return_entity_subwords_avg_vector":True,
         "raise_error_on_unknown_lemma":True
