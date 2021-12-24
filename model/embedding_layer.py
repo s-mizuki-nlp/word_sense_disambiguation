28372b006afc914b92bfbc887a8972afbe6dbdc3
Sakae Mizuki <mizuki.s.aa@m.titech.ac.jp>
2021-12-24 20:18:51 +0900

    feat: implemented additive feature along with pefixes.

--- model/embedding_layer.py
+++ model/embedding_layer.py
@@ -14,8 +14,20 @@ class HashCodeAwareEmbedding(BasePrefixAwareLayer):
     def __init__(self, n_seq_len: int, num_embeddings: int, embedding_dim: int, num_buckets: int,
                  num_hashes: int = 2, replace_trailing_zeroes: bool = False,
                  append_weight: bool = True,
+                 additive: bool = False,
                  **kwargs):
+        """
 
+        @param n_seq_len:
+        @param num_embeddings:
+        @param embedding_dim:
+        @param num_buckets:
+        @param num_hashes:
+        @param replace_trailing_zeroes:
+        @param append_weight:
+        @param additive: average along with prefixes (=sense hierarchy).
+        @param kwargs:
+        """
         super().__init__(replace_trailing_zeroes=replace_trailing_zeroes, null_prefix_index=0)
 
         self.emb_layer = HashEmbedding(num_embeddings=num_embeddings, num_hashes=num_hashes,
@@ -23,6 +35,7 @@ class HashCodeAwareEmbedding(BasePrefixAwareLayer):
                                        num_buckets=num_buckets, append_weight=append_weight,
                                        **kwargs)
         self.n_seq_len = n_seq_len
+        self._additive = additive
 
     def init_weights(self, *args, **kwargs):
         self.emb_layer.reset_parameters(std=0.1)
@@ -31,10 +44,20 @@ class HashCodeAwareEmbedding(BasePrefixAwareLayer):
         # input_sequence: (n_batch, n_digits_so_far)
         assert input_sequence.ndim == 2, f"unexpected dimension size: {input_sequence.ndim}"
 
+        n_digits_so_far = min(self.n_seq_len, input_sequence.shape[-1])
+
         # input_sequence_prefix_hashes: (n_batch, n_digits_so_far)
         input_sequence_prefix_indices = self.transform_sequence_to_prefix_indices(input_sequence)
         # t_emb: (n_batch, n_digits_so_far, n_emb)
         t_emb = self.emb_layer.forward(input_sequence_prefix_indices)
+
+        if self._additive:
+            # moving average from MSD to d-th digits.
+            t_emb = torch.cumsum(t_emb, dim=1)
+            # by dividing number of digits, it may avoid nan error.
+            # t_denom: (1, n_digits_so_far, 1)
+            t_denom = torch.arange(start=1, end=n_digits_so_far+1, device=t_emb.device).view(1, -1, 1)
+            t_emb = t_emb / t_denom
 
         return t_emb
 
